961c28ef799eaf4d45de996ac0f8f516388080f9
Kaniah Dunn <kaniahdunn@gmail.com>
2019-11-19 09:16:30 -0500

    db connected

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://jayden:april2017@cluster0-va8gd.mongodb.net/dashboard?retryWrites=true&w=majority', 
+    // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'dashboard'
+};
